677a31b3040bc9ee4ace58944546a1636b6a06ed
Liz <liz@liz-thinkpad.(none)>
2014-06-30 16:18:49 -0700

    fixing my repository\!

--- datastructures/simpexp.py
+++
@@ -1,6 +0,0 @@
-def compute_expression(expr):
-    # Write your code here
-    # To print results to the standard output you can use print
-    # Example: print "Hello world!"
-    # This desperately needs work! Looking into recursive descent parsing.
-    print eval(expr)
--- sorting/sortmerge.py
+++
@@ -1,9 +0,0 @@
-def merge_arrays(a, b):
-    # Write your code here
-    # To print results to the standard output you can use print
-    # Example: print "Hello world!"
-    
-    oneArray = sorted(a + b)
-    
-    for num in oneArray:
-        print num
